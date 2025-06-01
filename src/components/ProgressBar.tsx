143faea7efc6089a8976cb7ebc69a301f45f1bc8
andy-engineer009 <singhmak305@gmail.com>
2025-06-01 11:54:09 +0530

    2nd commit

--- src/components/ProgressBar.tsx
+++
@@ -1,9 +0,0 @@
-const ProgressBar = ({ completed, total }: { completed: number; total: number }) => {
-  const percent = (completed / total) * 100;
-  return (
-    <div className="progress-bar">
-      <div className="progress-bar-inner" style={{ width: `${percent}%` }} />
-    </div>
-  );
-};
-export default ProgressBar;
